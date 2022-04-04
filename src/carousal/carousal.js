6cfa9c72daa8eb0042a9b6cda9df1b20194227ac
arun436 <arunkumarchintha136@gmail.com>
2022-04-04 10:30:35 +0530

    Images

--- src/carousal/carousal.js
+++ src/carousal/carousal.js
@@ -11,6 +11,10 @@ import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
+import nature from '../carousal/nature.jpg'
+import plant from '../carousal/plant.jpg'
+import boat from '../carousal/boat.jpg'
+import mountain from '../carousal/mountain.jpg'
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
@@ -18,22 +22,22 @@ const images = [
   {
     label: 'San Francisco – Oakland Bay Bridge, United States',
     imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+      nature,
   },
   {
     label: 'Bird',
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+      plant,
   },
   {
     label: 'Bali, Indonesia',
     imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
+      boat,
   },
   {
     label: 'Goč, Serbia',
     imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+      mountain,
   },
 ];
 
